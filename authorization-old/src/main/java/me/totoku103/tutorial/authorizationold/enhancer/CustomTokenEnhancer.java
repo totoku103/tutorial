2e2a67e74b6012b43f73b12adbb7a9f1023d017d
KimSuhong <totoku103@humuson.com>
2022-10-05 18:05:02 +0900

    asdfasdf

--- authorization-old/src/main/java/me/totoku103/tutorial/authorizationold/enhancer/CustomTokenEnhancer.java
+++ authorization-old/src/main/java/me/totoku103/tutorial/authorizationold/enhancer/CustomTokenEnhancer.java
@@ -2,11 +2,13 @@ package me.totoku103.tutorial.authorizationold.enhancer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,6 +19,8 @@ public class CustomTokenEnhancer implements TokenEnhancer {
     public OAuth2AccessToken enhance(OAuth2AccessToken oAuth2AccessToken, OAuth2Authentication oAuth2Authentication) {
         Map<String, Object> additionalInfo = new HashMap<>();
         Authentication userAuthentication = oAuth2Authentication.getUserAuthentication();
+
+        // temp
         if (userAuthentication != null) {
             if (userAuthentication.getAuthorities().size() > 0) {
                 final String authority = userAuthentication.getAuthorities().stream().map(d -> d.getAuthority()).collect(Collectors.joining(" "));
@@ -25,6 +29,11 @@ public class CustomTokenEnhancer implements TokenEnhancer {
         } else {
             log.warn("userAuthentication is empty. {}", oAuth2Authentication.getName());
         }
+
+        // temp
+        Collection<GrantedAuthority> authorities = oAuth2Authentication.getAuthorities();
+        additionalInfo.put("authorities", authorities);
+
         ((DefaultOAuth2AccessToken) oAuth2AccessToken).setAdditionalInformation(additionalInfo);
         return oAuth2AccessToken;
     }
